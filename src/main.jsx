415dd7c3564563d88f210a94570deeec93aa67b2
Fouad Bechar <FBechar@outlook.com>
2025-09-24 21:06:46 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles/global.css'
+// import './styles/global.css'
 // Import original page styles and scripts extracted from the legacy index.html
 import './styles/original.css'
-import './original-scripts'
+// import './original-scripts'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
