24bbbcb5081eed999a58635bf6ee109785c912de
Fouad Bechar <FBechar@outlook.com>
2025-09-26 00:54:59 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-
+import image0104 from "/src/assets/images/image0104.webp";
+import image0105 from "/src/assets/images/image0105.webp";
 
 
 export default function Footer() {
 
     const footer = (
     <>
-   <center> <span> <a className="loadicon010101" href="https://www.facebook.com/FouadBechar2" target="_parent"><img className="img321" src="https://fouadbechar.x10.mx/p/m/image0104.webp" alt="image0102" width="30px" height="30px"/> </a> <a className="loadicon010101" href="https://x.com/FouadBechar" target="_parent"> <img className="im0im1" src="https://fouadbechar.x10.mx/p/m/image0105.webp" alt="image0105" width="30px" height="30px"/></a><a className="loadicon010101" href="https://www.youtube.com/channel/UCi3RVanUvgW2o1Ld5lt7EjA" target="_parent"><img className="im0im2" src="https://fouadbechar.x10.mx/p/m/youtb.webp" alt="image0105" width="43px" height="30px"/></a><img id="open-btn" src="https://fouadbechar.x10.mx/p/m/ml.webp" alt="email" width="30px" height="30px"/></span><div className="pr0101"><a href="p/v/pr" target="_parent">Privacy Policy</a></div></center>
+   <center> <span> <a className="loadicon010101" href="https://www.facebook.com/FouadBechar2" target="_parent"> <img className="img321" src={image0104} alt="image0102" width="30px" height="30px"/> </a> <a className="loadicon010101" href="https://x.com/FouadBechar" target="_parent"> <img className="im0im1" src={image0105} alt="image0105" width="30px" height="30px"/></a><a className="loadicon010101" href="https://www.youtube.com/channel/UCi3RVanUvgW2o1Ld5lt7EjA" target="_parent"><img className="im0im2" src="https://fouadbechar.x10.mx/p/m/youtb.webp" alt="image0105" width="43px" height="30px"/></a><img id="open-btn" src="https://fouadbechar.x10.mx/p/m/ml.webp" alt="email" width="30px" height="30px"/></span><div className="pr0101"><a href="p/v/pr" target="_parent">Privacy Policy</a></div></center>
    </>
     );
   return (
