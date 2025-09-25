751594d60dbb45493eed36b5902e005c551032a1
Fouad Bechar <FBechar@outlook.com>
2025-09-25 04:22:37 +0100

    Update ContentSections.jsx

--- src/components/ContentSections.jsx
+++ src/components/ContentSections.jsx
@@ -363,68 +363,7 @@ export default function ContentSections() {
     </>
   );
 
-  const section11 = (
-      <>
-    <div className="social-center">
-      <span>
-        <a
-          className="loadicon010101"
-          href="https://www.facebook.com/FouadBechar2"
-          target="_parent"
-          rel="noreferrer"
-        >
-          <img
-            className="img321"
-            src="https://fouadbechar.x10.mx/p/m/image0104.webp"
-            alt="image0102"
-            width={30}
-            height={30}
-          />
-        </a>
-        <a
-          className="loadicon010101"
-          href="https://x.com/FouadBechar"
-          target="_parent"
-          rel="noreferrer"
-        >
-          <img
-            className="im0im1"
-            src="https://fouadbechar.x10.mx/p/m/image0105.webp"
-            alt="image0105"
-            width={30}
-            height={30}
-          />
-        </a>
-        <a
-          className="loadicon010101"
-          href="https://www.youtube.com/channel/UCi3RVanUvgW2o1Ld5lt7EjA"
-          target="_parent"
-          rel="noreferrer"
-        >
-          <img
-            className="im0im2"
-            src="https://fouadbechar.x10.mx/p/m/youtb.webp"
-            alt="image0105"
-            width={43}
-            height={30}
-          />
-        </a>
-        <img
-          id="open-btn"
-          src="https://fouadbechar.x10.mx/p/m/ml.webp"
-          alt="email"
-          width={30}
-          height={30}
-        />
-      </span>
-      <div className="pr0101">
-        <a href="p/v/pr" target="_parent">
-          Privacy Policy
-        </a>
-      </div>
-    </div>
-    </>   
-  );
+ 
 
   useEffect(() => {
     const messages = {
@@ -626,7 +565,6 @@ export default function ContentSections() {
         </div>
       </div>
 
-      <div className="social-row">{section11}</div>
 
       <button id="chat-toggle" type="button">
         <img
