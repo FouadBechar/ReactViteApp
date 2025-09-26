521105ff879ce066e4bcdf6772f4ef3120ada0a6
Fouad Bechar <FBechar@outlook.com>
2025-09-26 11:48:41 +0100

    Create TextDq.jsx

---
+++ src/components/TextDq.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect} from "react";
+
+// import d1 from "/src/assets/images/d1.webp";
+// import image2ss from "/src/assets/images/image2ss.webp";
+// import image3ss from "/src/assets/images/image3ss.webp";
+
+
+export default function TextDq() {
+
+useEffect(() => {
+  
+
+  }, []);
+ 
+  return (
+    <>
+  
+  </>
+    
+  );
+}
