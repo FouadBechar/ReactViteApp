ce66de114c928ed1affc036eadd98f17aaf614e4
Fouad Bechar <FBechar@outlook.com>
2025-09-26 10:31:38 +0100

    Create Videoshow.jsx

---
+++ src/components/Videoshow.jsx
@@ -0,0 +1,32 @@
+import React, {useEffect} from "react";
+
+// import image1ss from "/src/assets/images/image1ss.webp";
+// import image2ss from "/src/assets/images/image2ss.webp";
+// import image3ss from "/src/assets/images/image3ss.webp";
+
+
+
+export default function Videoshow() {
+
+
+
+  
+
+ 
+
+useEffect(() => {
+ 
+   
+
+  }, []);
+ 
+
+
+  return (
+    <>
+ 
+  
+</>
+    
+  );
+}
