7e500b247b3f78fc75af428702b68947d9ff02a6
Fouad Bechar <FBechar@outlook.com>
2025-09-26 02:35:13 +0100

    Update Slideshow.jsx

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -6,76 +6,9 @@ import React, {useEffect} from "react";
 // import icons03 from "/src/assets/images/icons03.svg";
 
 
-
-      
-  let slideIndex = 1;
-  let slideInterval;
-  let isPaused = false;
-
-  function plusSlides(n) {
-    if (!isPaused) {
-      clearInterval(slideInterval);
-      showSlides((slideIndex += n));
-      slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
-    }
-  }
-
-  const prevButton = document.querySelector(".prev");
-  const nextButton = document.querySelector(".next");
-  prevButton.addEventListener("click", () => plusSlides(-1));
-  nextButton.addEventListener("click", () => plusSlides(1));
-
-  function currentSlide(n) {
-    if (!isPaused) {
-      clearInterval(slideInterval);
-      showSlides((slideIndex = n));
-      slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
-    }
-  }
-
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-  }
-
-  document.addEventListener("DOMContentLoaded", (event) => {
-    showSlides(slideIndex);
-    slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
-  });
-
-  document.querySelectorAll(".mySlides").forEach((img) => {
-    img.addEventListener("mouseover", () => {
-      clearInterval(slideInterval);
-      isPaused = true;
-    });
-
-    img.addEventListener("mouseout", () => {
-      isPaused = false;
-      slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
-    });
-  });
-
-
-
-
 export default function Slideshow() {
 
-  onst section8 = (
+ const section8 = (
     <>
       <div className="mySlides fade">
         <div className="numbertext">1 / 3</div>
@@ -160,10 +93,70 @@ export default function Slideshow() {
   );
 
  
+useEffect(() => {
+   let slideIndex = 1;
+  let slideInterval;
+  let isPaused = false;
 
-  
+  function plusSlides(n) {
+    if (!isPaused) {
+      clearInterval(slideInterval);
+      showSlides((slideIndex += n));
+      slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
+    }
+  }
 
- 
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  prevButton.addEventListener("click", () => plusSlides(-1));
+  nextButton.addEventListener("click", () => plusSlides(1));
+
+  function currentSlide(n) {
+    if (!isPaused) {
+      clearInterval(slideInterval);
+      showSlides((slideIndex = n));
+      slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
+    }
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+
+  document.addEventListener("DOMContentLoaded", (event) => {
+    showSlides(slideIndex);
+    slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
+  });
+
+  document.querySelectorAll(".mySlides").forEach((img) => {
+    img.addEventListener("mouseover", () => {
+      clearInterval(slideInterval);
+      isPaused = true;
+    });
+
+    img.addEventListener("mouseout", () => {
+      isPaused = false;
+      slideInterval = setInterval(() => showSlides((slideIndex += 1)), 10000);
+    });
+  });
+
+  }, []);
 
   return (
     
