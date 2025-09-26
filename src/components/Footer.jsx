27a5d854200e517a8db4459b867aafbca415c384
Fouad Bechar <FBechar@outlook.com>
2025-09-26 01:15:10 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
     const footer = (
     <>
-   <center> <span> <a className="loadicon010101" href="https://www.facebook.com/FouadBechar2" target="_parent"> <img className="img321" src={image0104} alt="image0102" width="30px" height="30px"/> </a> <a className="loadicon010101" href="https://x.com/FouadBechar" target="_parent"> <img className="im0im1" src={image0105} alt="image0105" width="30px" height="30px"/></a><a className="loadicon010101" href="https://www.youtube.com/channel/UCi3RVanUvgW2o1Ld5lt7EjA" target="_parent"><img className="im0im2" src={youtb} alt="image0105" width="43px" height="30px"/></a><img id="open-btn" src={ml} alt="email" width="30px" height="30px"/></span><div className="pr0101"><a href="pr.html" target="_parent">Privacy Policy</a></div></center>
+   <center> <span> <a className="loadicon010101" href="https://www.facebook.com/FouadBechar2" target="_parent"> <img className="img321" src={image0104} alt="image0102" width="30px" height="30px"/> </a> <a className="loadicon010101" href="https://x.com/FouadBechar" target="_parent"> <img className="im0im1" src={image0105} alt="image0105" width="30px" height="30px"/></a><a className="loadicon010101" href="https://www.youtube.com/channel/UCi3RVanUvgW2o1Ld5lt7EjA" target="_parent"><img className="im0im2" src={youtb} alt="image0105" width="43px" height="30px"/></a><img id="open-btn" src={ml} alt="email" width="30px" height="30px"/></span><div className="pr0101"><a href="PrivacyPolicy.html" target="_parent">Privacy Policy</a></div></center>
    </>
     );
   return (
