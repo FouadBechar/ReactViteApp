76f00c22c942d264c78a7557793f27ca9dd2d2cb
Fouad Bechar <FBechar@outlook.com>
2025-09-26 10:43:07 +0100

    Update Videoshow.jsx

--- src/components/Videoshow.jsx
+++ src/components/Videoshow.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 
-// import image1ss from "/src/assets/images/image1ss.webp";
+import image1 from "/src/assets/images/image1.webp";
 // import image2ss from "/src/assets/images/image2ss.webp";
 // import image3ss from "/src/assets/images/image3ss.webp";
 
@@ -15,7 +15,39 @@ export default function Videoshow() {
  
 
 useEffect(() => {
- 
+
+  var vido;
+  var clo;
+  var im00;
+  var aaa001;
+  var divvv12;
+
+  function video() {
+    vido = document.getElementById("vid");
+    clo = document.getElementById("cl");
+    im00 = document.getElementById("im0");
+    aaa001 = document.getElementById("aa001");
+    divvv12 = document.getElementById("divv12");
+    vido.src =
+      "https://res.cloudinary.com/dougpslkv/video/upload/v1708992004/WWF.mp4";
+    clo.style.display = "block";
+    vido.style.display = "block";
+    aaa001.style.display = "block";
+    im00.style.display = "none";
+    divvv12.style.display = "none";
+  }
+
+  function closse() {
+    vido = document.getElementById("vid");
+    clo = document.getElementById("cl");
+    clo.style.display = "none";
+    vido.style.display = "none";
+    aaa001.style.display = "none";
+    im00.style.display = "block";
+    divvv12.style.display = "block";
+    vido.src = "";
+  }
+
    
 
   }, []);
