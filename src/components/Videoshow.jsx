78219bc80b6eac570699a5a05fe883a0d84639a7
Fouad Bechar <FBechar@outlook.com>
2025-09-26 10:37:20 +0100

    Update Videoshow.jsx

--- src/components/Videoshow.jsx
+++ src/components/Videoshow.jsx
@@ -24,7 +24,28 @@ useEffect(() => {
 
   return (
     <>
- 
+  <div className="wrapper010" />
+      <div className="open01">
+        <img
+          className="grid1img"
+          src={image1}
+          width="200px"
+          height="133px"
+          alt="image"
+        />
+      </div>
+
+      <div className="wrapper">
+        <video className="vividd" src="" preload="auto" controls autoPlay />
+        <div className="close">
+          <a href="https://www.worldwildlife.org/" target="_parent">
+            <button type="button" className="lien">
+              WWF
+            </button>
+          </a>
+          <button>Close</button>
+        </div>
+      </div>
   
 </>
     
