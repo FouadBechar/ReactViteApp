e89ea3ed22db9bf780bd177dc3305ff9c1434549
Fouad Bechar <FBechar@outlook.com>
2025-10-26 03:10:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import NavBar from './components/NavBar';
 import ContentSections from './components/ContentSections';
 import Footer from './components/Footer';
 
-// import Register from './components/Register'
-// import Login from './components/Login'
+import Register from './components/Register'
+import Login from './components/Login'
 
 export default function App() {
-  // const path = typeof window !== 'undefined' ? window.location.pathname : '/'
-  // if (path === '/register' || path === '/register.html') {
-  //   return <Register />
-  // }
-  // if (path === '/login' || path === '/login.html') {
-  //   return <Login />
-  // }
+  const path = typeof window !== 'undefined' ? window.location.pathname : '/'
+  if (path === '/register' || path === '/register.html') {
+    return <Register />
+  }
+  if (path === '/login' || path === '/login.html') {
+    return <Login />
+  }
 
   return (
     <div className="app-root">
